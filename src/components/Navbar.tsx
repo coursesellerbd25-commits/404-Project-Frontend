d24f95b8b08f63734d68c4c981006dab59610eb1
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-07-10 16:53:06 +0600

    Update frontend

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,10 +47,10 @@ export default function Navbar() {
         <NavLink
           to="/annotations"
           className={({ isActive }) =>
-            `text-lg transition ${
+            `pb-2 text-lg transition ${
               isActive
-                ? "font-semibold text-black border-b-4 border-black"
-                : "text-gray-700 border-b-4 border-transparent hover:text-black"
+                ? "font-semibold text-black border-b-[5px] border-black"
+                : "text-gray-700 border-b-[5px] border-transparent hover:text-black"
             }`
           }
         >
