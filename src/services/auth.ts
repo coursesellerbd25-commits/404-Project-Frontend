d0d2fc8f246dbdec4b32e268208a500fe72532f3
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-07-11 18:10:20 +0600

    Update Login Page

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const API = import.meta.env.VITE_API_URL;
 
-export const loginUser = async (email: string, password: string) => {
+export const loginUser = async (
+  email: string,
+  password: string
+) => {
   const res = await axios.post(`${API}/api/login/`, {
     email,
     password,
