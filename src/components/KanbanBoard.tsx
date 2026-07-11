9e41ad331201e27422c811e5b0add0dab187ebd4
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-07-11 21:05:37 +0600

    Update Kanban Board

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,6 +1,14 @@
-import { DndContext } from "@dnd-kit/core";
+import { DndContext,  DragOverlay, } from "@dnd-kit/core";
 import Column from "./tasks/Column";
 import { updateTask } from "../services/task";
+import { useState } from "react";
+import TaskCard from "./tasks/TaskCard";
+import {
+  TouchSensor,
+  MouseSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 
 export default function KanbanBoard({
   tasks,
@@ -9,9 +17,22 @@ export default function KanbanBoard({
   tasks: any[];
   refresh: () => void;
 }) {
+  const sensors = useSensors(
+    useSensor(MouseSensor),
+    useSensor(TouchSensor)
+  );
+  const [activeTask, setActiveTask] = useState<any>(null);
+  const handleDragStart = (event:any) => {
+    const task = tasks.find(
+      (task)=> task.id === event.active.id
+    );
+
+    setActiveTask(task);
+  };
   const handleDragEnd = async (event: any) => {
     const { active, over } = event;
 
+    setActiveTask(null);
     if (!over) return;
 
     const taskId = active.id;
@@ -29,7 +50,7 @@ export default function KanbanBoard({
   };
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
+    <DndContext sensors={sensors} onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
       <div className="flex flex-col gap-8 sm:gap-10 lg:gap-12 mt-6">
         <Column
           title="To Do"
@@ -52,6 +73,16 @@ export default function KanbanBoard({
           refresh={refresh}
         />
       </div>
+      <DragOverlay>
+        {
+          activeTask && (
+            <TaskCard
+              task={activeTask}
+              refresh={refresh}
+            />
+          )
+        }
+      </DragOverlay>
     </DndContext>
   );
 }
